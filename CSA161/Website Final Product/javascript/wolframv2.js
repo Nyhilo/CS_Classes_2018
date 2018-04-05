95bdfa27f1d232228ad5f4b3ed3741217a1c0ce0
nyhilo <silentclowd@gmail.com>
2018-04-05 16:23:07 -0700

    Clicking functionality for the set row done.

--- CSA161/Website Final Product/javascript/wolframv2.js
+++ CSA161/Website Final Product/javascript/wolframv2.js
@@ -3,7 +3,7 @@ Rewriting Wolfram.js for an html canvas, because doing it with fonts is badly no
 
 
 
-Jacob CLoward 2/13/18
+Jacob CLoward 4/5/18
 */
 
 // window.onload = function() {
@@ -18,6 +18,7 @@ let context         = simField.getContext("2d")
 let simInput        = document.getElementById("sim-first-line");
 let inContext       = simInput.getContext("2d")
 
+
 // Grid width and height, this will probably become more flexible later
 let field       = new Array();
 let inField     = new Array();
@@ -25,16 +26,20 @@ let width       = simField.width;
 let height      = simField.height;
 let inWidth     = simInput.width;
 let inHeight    = simInput.height;
-let pixelSize   = 10;
+let cellSize   = 15;
 
 // Grid styling
-const gridColor       = "#dddddd";
-const pixelColor      = "#000000"
+// const gridColor       = "#dddddd";
+// const gridColor       = "white"
+const gridColor       = "#F7F5EF"
+const cellColor      = "#000000"
 const gridThickness   = 2;
 
 // There are 8 rules for the simulation. We'll define rule 30 as an initial state just for fun
 // The array elements correspond to the rules [111,110,101,100,011,010,001,000]
 let rules = [0,0,0,1,1,1,1,0];
+
+
 
 
   ////////////////////
@@ -47,12 +52,12 @@ function clearBoard() {
     context.clearRect(0, 0, simField.width, simField.height);
 
     // Now we draw the rest of the board
-    for (let x = 0; x <= width; x+=pixelSize) {
+    for (let x = 0; x <= width; x+=cellSize) {
         context.moveTo(x,0);
         context.lineTo(x,height);
     }
 
-    for (let y = 0; y <= height; y+=pixelSize) {
+    for (let y = 0; y <= height; y+=cellSize) {
         context.moveTo(0,y);
         context.lineTo(width,y)   
     }
@@ -69,8 +74,8 @@ function clearInput() {
     inContext.clearRect(0, 0, simInput.width, simInput.height);
     
     // Draw the first line board.
-    // The first line is garanteed to be one pixel deep so we don't need complex vertical lines
-    for (let x = 0; x <= inWidth; x+=pixelSize) {
+    // The first line is garanteed to be one cell deep so we don't need complex vertical lines
+    for (let x = 0; x <= inWidth; x+=cellSize) {
         inContext.moveTo(x,0);
         inContext.lineTo(x,inHeight);
     }
@@ -88,35 +93,31 @@ function clearInput() {
     inContext.stroke();
 }
 
-function drawPixel(ctx, i, j) {
-    ctx.fillRect(j*10+1,(i)*10+1,8,8);
-}
+function drawCell(ctx, i, j) { ctx.fillRect( j*cellSize+1, (i)*cellSize+1, cellSize-2, cellSize-2 ); }
 
-function drawPixels() {
+function drawBoard() {
     // Clear board
     clearBoard();
     // Smol variables
-    let h = height/pixelSize;
-    let w = width/pixelSize;
+    let h = height/cellSize;
+    let w = width/cellSize;
 
     for (let i = 0; i < h; i++) {
         for (let j = 0; j < w; j++) {
             if ( field[i][j] == 1) {
-                drawPixel(context, i, j);
+                drawCell(context, i, j);
             }
         }
     }
-
 }
 
 function drawInput() {
     clearInput();
-    let w = width/pixelSize;
+    let w = width/cellSize;
 
     for (var j = 0; j < w; j++) {
         if ( inField[j] == 1 ) {
-            console.log(j);
-            drawPixel(inContext, 0, j);
+            drawCell(inContext, 0, j);
         }
     }
 }
@@ -146,14 +147,39 @@ function createArray(dimensions) {
 }
 
 
+  //////////////////////////
+ // Interation Functions //
+//////////////////////////
+
+
+
+simInput.addEventListener('click', (e) => {
+    // We need to get the position of the canvas in order to click it properly
+    let simInputEdge = simInput.getBoundingClientRect();
+
+    const clickPosition = e.clientX;
+    const relativePosition = clickPosition - simInputEdge.left;
+
+    // The cell we clicked on is the relative position (by pixel) size of a cell
+    position = Math.ceil(relativePosition / cellSize) - 1; // Though arrays start at 0, hence -1
+
+    // Reverse the value of the clicked cell and redraw the board
+    // if x=0, 1-(x) = 1  |  if x=1, 1-(x) = 0. Neat. 
+    inField[position] = 1 - inField[position] 
+    drawInput();
+
+});
+
+
+
   //////////
  // Main //
 //////////
 
 // Create a 2d array that fits in the canvas
-field = createArray([height/pixelSize, width/pixelSize]);
+field = createArray([height/cellSize, width/cellSize]);
 // Create a 1d array for the input feild. It will be initialized randomly
-inField = createArray([width/pixelSize]);
+inField = createArray([width/cellSize]);
 for (var i = 0; i < inField.length; i++) {
     inField[i] = Math.round(Math.random())
 }
@@ -170,10 +196,7 @@ field[0] = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
 field[1] = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1];
 field[field.length-1] = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
 
-console.log(field)
-clearBoard();
-clearInput();
-drawPixels();
+drawBoard();
 drawInput();
 
 // }    // Closing the DOM loader at the top
