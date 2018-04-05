4201367d336b6d4047073453c93116264157fb60
nyhilo <silentclowd@gmail.com>
2018-04-05 14:48:31 -0700

    fixed scope issues with the array size. Set up proper drawing for the input line. Changed all var keywords to let keywords.

--- CSA161/Website Final Product/javascript/wolframv2.js
+++ CSA161/Website Final Product/javascript/wolframv2.js
@@ -13,65 +13,46 @@ Jacob CLoward 2/13/18
 /////////////
 
 // Shortcuts for html elements
-var simField        = document.getElementById("sim-area");
-var context         = simField.getContext("2d")
-var simFirstLine    = document.getElementById("sim-first-line");
-var flContext       = simFirstLine.getContext("2d")
+let simField        = document.getElementById("sim-area");
+let context         = simField.getContext("2d")
+let simInput        = document.getElementById("sim-first-line");
+let inContext       = simInput.getContext("2d")
 
 // Grid width and height, this will probably become more flexible later
-var field       = new Array();
-var width       = simField.width;
-var height      = simField.height;
-var flWidth     = simFirstLine.width;
-var flHeight    = simFirstLine.height;
-var pixelSize   = 10;
+let field       = new Array();
+let inField     = new Array();
+let width       = simField.width;
+let height      = simField.height;
+let inWidth     = simInput.width;
+let inHeight    = simInput.height;
+let pixelSize   = 10;
 
 // Grid styling
-var gridColor       = "#dddddd";
-var pixelColor      = "#000000"
-var gridThickness   = 2;
+const gridColor       = "#dddddd";
+const pixelColor      = "#000000"
+const gridThickness   = 2;
 
 // There are 8 rules for the simulation. We'll define rule 30 as an initial state just for fun
 // The array elements correspond to the rules [111,110,101,100,011,010,001,000]
-var rules = [0,0,0,1,1,1,1,0];
+let rules = [0,0,0,1,1,1,1,0];
 
 
   ////////////////////
  // Draw Functions //
 ////////////////////
 
-function drawBoard() {
-    // Frist we clear both boards
+function clearBoard() {
+    // Frist we clear the board
     context.beginPath();
-    flContext.beginPath();
     context.clearRect(0, 0, simField.width, simField.height);
-    flContext.clearRect(0, 0, simFirstLine.width, simFirstLine.height);
-    // Draw the first line board.
-    // The first line is garanteed to be one pixel deep so we don't need complex vertical lines
-    for (var x = 0; x <= flWidth; x+=pixelSize) {
-        flContext.moveTo(x,0);
-        flContext.lineTo(x,flHeight);
-    }
-
-    // Draw the top and bottom lines of the first line box
-    flContext.moveTo(0,0);
-    flContext.lineTo(flWidth,0);
-
-    flContext.moveTo(0,flHeight);
-    flContext.lineTo(flWidth,flHeight)
-
-    // Stroke the first line box lines
-    flContext.strokeStyle = gridColor;
-    flContext.lineWidth = gridThickness;
-    flContext.stroke();
 
     // Now we draw the rest of the board
-    for (var x = 0; x <= width; x+=pixelSize) {
+    for (let x = 0; x <= width; x+=pixelSize) {
         context.moveTo(x,0);
         context.lineTo(x,height);
     }
 
-    for (var y = 0; y <= height; y+=pixelSize) {
+    for (let y = 0; y <= height; y+=pixelSize) {
         context.moveTo(0,y);
         context.lineTo(width,y)   
     }
@@ -82,54 +63,117 @@ function drawBoard() {
     context.stroke();
 }
 
-function drawPixels() {
-    drawBoard();
-    h = height/pixelSize;
-    w = width/pixelSize;
+function clearInput() {
+    // Frist we clear the input board
+    inContext.beginPath(); 
+    inContext.clearRect(0, 0, simInput.width, simInput.height);
+    
+    // Draw the first line board.
+    // The first line is garanteed to be one pixel deep so we don't need complex vertical lines
+    for (let x = 0; x <= inWidth; x+=pixelSize) {
+        inContext.moveTo(x,0);
+        inContext.lineTo(x,inHeight);
+    }
 
-    for (var i = 1; i < h; i++) {
-        for (var j = 0; j < w; j++) {
+    // Draw the top and bottom lines of the first line box
+    inContext.moveTo(0,0);
+    inContext.lineTo(inWidth,0);
+
+    inContext.moveTo(0,inHeight);
+    inContext.lineTo(inWidth,inHeight)
+
+    // Stroke the first line box lines
+    inContext.strokeStyle = gridColor;
+    inContext.lineWidth = gridThickness;
+    inContext.stroke();
+}
+
+function drawPixel(ctx, i, j) {
+    ctx.fillRect(j*10+1,(i)*10+1,8,8);
+}
+
+function drawPixels() {
+    // Clear board
+    clearBoard();
+    // Smol variables
+    let h = height/pixelSize;
+    let w = width/pixelSize;
+
+    for (let i = 0; i < h; i++) {
+        for (let j = 0; j < w; j++) {
             if ( field[i][j] == 1) {
-                context.fillRect(j*10+1,(i-1)*10+1,8,8);
+                drawPixel(context, i, j);
             }
         }
     }
 
 }
 
+function drawInput() {
+    clearInput();
+    let w = width/pixelSize;
+
+    for (var j = 0; j < w; j++) {
+        if ( inField[j] == 1 ) {
+            console.log(j);
+            drawPixel(inContext, 0, j);
+        }
+    }
+}
+
   ///////////////////////
  // General Functions //
 ///////////////////////
 
-// Stolen from stack overflow
 // Allows us to really quickly generate an array of arbitrary multidimensional size
-function createArray(length) {
-    var arr = new Array(length || 0),
-        i = length;
+// Takes an array of dimension sizes as an input
+// For example, an input of [2,3,5] with create a 3d array of size 2x3x5
+function createArray(dimensions) {
+    // Pulls out the first dimentional length
+    let [length, ...rest] = dimensions;
+    // Creates our return array
+    let arr = new Array(length || 0);
 
-    if (arguments.length > 1) {
-        var args = Array.prototype.slice.call(arguments, 1);
-        while(i--) arr[length-1 - i] = createArray.apply(this, args);
-    };
+    // If there are more dimentions in the input array, create a sub-array for each index in the current array
+    // and run the same formula for each of those. This is what lets us generate an array of arbitrary dimensions
+    if (rest.length) {
+        for (let i = 0; i < length; i++) {
+            arr[i] = createArray(rest);
+        }
+    }
 
     return arr;
-};
+}
 
 
   //////////
  // Main //
 //////////
 
-field = createArray(height/pixelSize+1, width/pixelSize)
+// Create a 2d array that fits in the canvas
+field = createArray([height/pixelSize, width/pixelSize]);
+// Create a 1d array for the input feild. It will be initialized randomly
+inField = createArray([width/pixelSize]);
+for (var i = 0; i < inField.length; i++) {
+    inField[i] = Math.round(Math.random())
+}
+
+
 // Generate a random field for testing
-for (var i = 0; i < height/pixelSize+1; i++) {
-    for (var j = 0; j < width/pixelSize; j++) {
+for (let i = 0; i < field.length; i++) {
+    // field[0].length effectively gets the "width" of our 2d array
+    for (let j = 0; j < field[0].length; j++) {
         field[i][j] = Math.round(Math.random());
     }
 }
-field[0][0] = [1,1]
+field[0] = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1];
+field[1] = [1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1];
+field[field.length-1] = [1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1];
+
 console.log(field)
-drawBoard();
+clearBoard();
+clearInput();
 drawPixels();
+drawInput();
 
 // }    // Closing the DOM loader at the top
